f93c273cc1e137a3ff811b181de6b60085cddbda
Thomas <jnolen7@msn.com>
2024-10-31 08:46:35 -0400

    Uses environmental variables to login. Starts bookings for 9 AM.

--- book_subs_script.py
+++ book_subs_script.py
@@ -1,6 +1,7 @@
 import time
 import os
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
